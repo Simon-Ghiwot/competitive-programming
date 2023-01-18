1e1e0db313436b43feb7678f6708f03cc7213742
Simon-Ghiwot <97943028+Simon-Ghiwot@users.noreply.github.com>
2023-01-18 19:38:27 +0300

    Create Find the Winner of the Circular Game.py

---
+++ Find the Winner of the Circular Game.py
@@ -0,0 +1,11 @@
+class Solution:
+    def findTheWinner(self, n: int, k: int) -> int:
+        arr = [i for i in range(1, n + 1)]
+        
+        i = 0
+        while len(arr) != 1:
+            i = (i + k - 1) % len(arr)
+            poped = arr.pop(i) 
+            
+        return arr[0]
+    
