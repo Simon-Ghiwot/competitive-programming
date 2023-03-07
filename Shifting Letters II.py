83b95af16a918550bc4e4f6c279ba18a3f7ec17b
Simon-Ghiwot <97943028+Simon-Ghiwot@users.noreply.github.com>
2023-03-07 22:20:02 +0300

    Update Shifting Letters II.py

--- Shifting Letters II.py
+++ Shifting Letters II.py
@@ -1,21 +1,21 @@
 class Solution:
     def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
         s_ord = [ord(i) for i in s]
-        final = [0] * (len(s) + 1)
+        line = [0] * (len(s) + 1)
 
-        for shift in shifts:
-            if shift[2] == 1:
-                final[shift[0]] += 1
-                final[shift[1] + 1] -= 1
+        for start, end, direction in shifts:
+            if direction == 1:
+                line[start] += 1
+                line[end + 1] -= 1
             else:
-                final[shift[0]] -= 1
-                final[shift[1] + 1] += 1
+                line[start] -= 1
+                line[end + 1] += 1
 
         result = []
-        for i in range(1, len(final)):
-            final[i] += final[i - 1]
-        for i in range(len(final) - 1):
-            char = (s_ord[i] + final[i] - 97 + 26) % 26 + 97
+        for i in range(1, len(line)):
+            line[i] += line[i - 1]
+        for i in range(len(line) - 1):
+            char = (s_ord[i] + line[i] - 97 + 26) % 26 + 97
             result.append(chr(char))
 
         return "".join(result)
