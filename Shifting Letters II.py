9b7d72c343dd8591acbf03bc963503a7724f1831
Simon-Ghiwot <97943028+Simon-Ghiwot@users.noreply.github.com>
2023-03-07 18:47:18 +0300

    Create Shifting Letters II.py

---
+++ Shifting Letters II.py
@@ -0,0 +1,21 @@
+class Solution:
+    def shiftingLetters(self, s: str, shifts: List[List[int]]) -> str:
+        s_ord = [ord(i) for i in s]
+        final = [0] * (len(s) + 1)
+
+        for shift in shifts:
+            if shift[2] == 1:
+                final[shift[0]] += 1
+                final[shift[1] + 1] -= 1
+            else:
+                final[shift[0]] -= 1
+                final[shift[1] + 1] += 1
+
+        result = []
+        for i in range(1, len(final)):
+            final[i] += final[i - 1]
+        for i in range(len(final) - 1):
+            char = (s_ord[i] + final[i] - 97) % 26 + 97
+            result.append(chr(char))
+
+        return "".join(result)
