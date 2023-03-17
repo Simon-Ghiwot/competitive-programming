c8a5d2527770861c7fb13a1bb99eb611b66d80b2
Simon-Ghiwot <97943028+Simon-Ghiwot@users.noreply.github.com>
2023-03-17 23:07:02 +0300

    Update Find Right Interval.py

--- Find Right Interval.py
+++ Find Right Interval.py
@@ -1,10 +1,10 @@
 class Solution:
     def findRightInterval(self, intervals: List[List[int]]) -> List[int]:
-        n, interval_index = len(intervals), []
+        n, interval_with_index = len(intervals), []
 
         for i in range(n):
-            interval_index.append([*intervals[i], i])
-        interval_index.sort()
+            interval_with_index.append([*intervals[i], i])
+        interval_with_index.sort()
         result = [0] * n
 
         def search(end_interval, start_index):
@@ -13,15 +13,15 @@ class Solution:
 
             while left <= right:
                 mid = (left + right) // 2
-                if interval_index[mid][0] < end_interval:
+                if interval_with_index[mid][0] < end_interval:
                     left = mid + 1
                 else:
-                    ans = interval_index[mid][2]
+                    ans = interval_with_index[mid][2]
                     right = mid - 1
             return -1 if ans == 10**7 else ans
 
         for i in range(n):
-            start, end, index = interval_index[i]
+            start, end, index = interval_with_index[i]
             result[index] = search(end, i)
 
         return result
