fe81cb9d1d839dfe4a31d28a19ce9c1a26db05e9
Simon-Ghiwot <97943028+Simon-Ghiwot@users.noreply.github.com>
2023-04-06 13:05:58 +0300

    Create Fair Distribution of Cookies.py

---
+++ Fair Distribution of Cookies.py
@@ -0,0 +1,18 @@
+class Solution:
+    def distributeCookies(self, cookies: List[int], k: int) -> int:
+        if len(cookies) == k:
+            return max(cookies)
+        self.ans = 10e6 + 600
+        children = [0] * k
+        self.backtrack(cookies, children, 0)
+        return self.ans
+
+    def backtrack(self, cookies, children, index):
+        if index == len(cookies):
+            self.ans = min(self.ans, max(children))
+            return
+        for i in range(len(children)):
+            children[i] += cookies[index]
+            self.backtrack(cookies, children, index + 1)
+            children[i] -= cookies[index]
+        
