3e1bb4e45a62da8c504d8792818d2109e0f6d029
Simon-Ghiwot <97943028+Simon-Ghiwot@users.noreply.github.com>
2023-05-17 20:43:41 +0300

    Create Maximum Depth of N-ary Tree.py

---
+++ Maximum Depth of N-ary Tree.py
@@ -0,0 +1,16 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+class Solution:
+    def maxDepth(self, root: 'Node') -> int:
+        if not root:
+            return 0
+        max_ = 0
+        for child in root.children:
+            max_ = max(max_, self.maxDepth(child))
+        return max_ + 1
