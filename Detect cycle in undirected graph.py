dbff6932ed5869ab286a96bc817740ce99e892c8
Simon-Ghiwot <97943028+Simon-Ghiwot@users.noreply.github.com>
2023-06-23 20:47:42 +0300

    Update Detect cycle in undirected graph.py

--- Detect cycle in undirected graph.py
+++ Detect cycle in undirected graph.py
@@ -1,20 +1,20 @@
 class Solution:
-    #Function to detect cycle in an undirected graph.
+    	#Function to detect cycle in an undirected graph.
 	def isCycle(self, V: int, adj: List[List[int]]) -> bool:
-    #Code here
+    	#Code here
 		visited = [False] * len(adj)
-                def dfs(at, parent, visited):
-                      if visited[at]:
-                           return True
-                      visited[at] = True
-                      for to in adj[at]:
-                           if to == parent:
-                                continue
-                           if dfs(to, at, visited):
-                                return True
-                       return False
-                 for i in range(len(adj)):
-                       if visited[i]: continue
-                       if dfs(i, i, visited):
-                             return True
-                 return False
+		def dfs(at, parent, visited):
+		        if visited[at]:
+		   		return True
+		        visited[at] = True
+		        for to in adj[at]:
+				if to == parent:
+					continue
+			   	if dfs(to, at, visited):
+					return True
+	       		return False
+	 	for i in range(len(adj)):
+		       	if visited[i]: continue
+		       	if dfs(i, i, visited):
+		     		return True
+	 	return False
