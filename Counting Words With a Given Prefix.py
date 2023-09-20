924ed66f67090d802c080ebc417d269edbb0fd52
Simon-Ghiwot <97943028+Simon-Ghiwot@users.noreply.github.com>
2023-09-20 14:29:29 +0300

    Create Counting Words With a Given Prefix.py

---
+++ Counting Words With a Given Prefix.py
@@ -0,0 +1,10 @@
+class Solution:
+    def prefixCount(self, words: List[str], pref: str) -> int:
+        ans =  0
+
+        for word in words:
+            if pref == word[:len(pref)]:
+                ans += 1
+
+        return ans 
+        
